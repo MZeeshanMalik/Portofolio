c84cd2f0783c056bb2aadac03a1c3e5c3833b70a
M zeeshan <m121zeeshan@gmail.com>
2024-11-06 21:35:00 +0500

    modified

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   env: {
     DATABASE: process.env.DATABASEURL, // pulls from .env file
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
